11ec24e0a926eca29c78498cca6ae9b73cacaae3
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-24 11:23:45 +0530

    feat: cli argument parsing

---
+++ src/cli.rs
@@ -0,0 +1,51 @@
+use std::path::PathBuf;
+pub struct Arguments{
+    pub url : String,
+    pub dest_path : Option<PathBuf>,
+    pub help : bool,
+    pub version : bool,
+}
+
+pub fn parse_args(args: &[String]) -> Result<Arguments, String> {
+    if args.len() == 2 {
+        match args[1].as_str() {
+            "--help" | "-h" => {
+                return Ok(Arguments {
+                    url: String::new(),
+                    dest_path: None,
+                    help: true,
+                    version: false,
+                });
+            }
+
+            "--version" | "-v" => {
+                return Ok(Arguments {
+                    url: String::new(),
+                    dest_path: None,
+                    help: false,
+                    version: true,
+                });
+            }
+
+            _ => {}
+        }
+    }
+
+    match args.len() {
+        2 => Ok(Arguments {
+            url: args[1].clone(),
+            dest_path: None,
+            help: false,
+            version: false,
+        }),
+
+        3 => Ok(Arguments {
+            url: args[1].clone(),
+            dest_path: Some(PathBuf::from(&args[2])),
+            help: false,
+            version: false,
+        }),
+
+        _ => Err(String::from("Invalid format")),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ mod error;
 mod headers;
 mod http;
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String>  = std::env::args().collect();
+    println!("{:?}", args);
 }
