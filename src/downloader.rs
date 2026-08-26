85d23a3050ce2bd6541196c2458d5bf2cf1f5397
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-26 11:09:31 +0530

    feat: download stream implemented

--- src/downloader.rs
+++ src/downloader.rs
@@ -1,4 +1,6 @@
 use std::path::Path;
+use futures_util::StreamExt; //for stream nd next()
+use std::io::Write;
 
 use crate::error::Error;
 
@@ -7,11 +9,16 @@ fn init_file(dest: &Path) -> Result<std::fs::File, Error>{
     Ok(fileout)
 }
 pub async fn download(url: &str, out : &Path) -> Result<(), Error>{
-    
+        
     Ok(())
 }
 
-async fn download_stream(response: reqwest::Response, fileout: &mut std::fs::File) -> Result< (), Error>{
-    
+async fn download_stream(response: reqwest::Response, fileout: &mut std::fs::File) -> Result< (), Box<dyn std::error::Error>>{ //box dyn error just for now later will switch to my Error
+    let mut stream = response.bytes_stream();
+
+    while let Some(chunk) = stream.next().await{
+        let chunk = chunk?; //since chunk is still a result from reqwest, this unwraps it to &[u8]
+        fileout.write_all(&chunk)?;
+    }
     Ok(())
 }
